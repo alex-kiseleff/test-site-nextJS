c7dcbfac24a8a14cec5ead6dc65b68cabd7b83ad
Alexey <64145913+alex-kiseleff@users.noreply.github.com>
2022-07-27 21:13:39 +0400

    fix: Исправил получение данных.
    Для успешного деплоя, сделал получение данных статично, без fetch-запроса, из файла DATA_BASE.json.

--- src/pages/news/[id].tsx
+++ src/pages/news/[id].tsx
@@ -10,6 +10,7 @@ import NewsBlockComponent, {
 	INewsBlock,
 } from '../../components/NewsBlockComponent/NewsBlockComponent';
 import { Container, Section } from '../../styles/pages/news.styles';
+import db from '../../DATA_BASE.json';
 
 interface IData {
 	news: Array<INewsBlock>;
@@ -25,18 +26,19 @@ const NewsComponent: NextPage<IData> = ({ news }): JSX.Element => {
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	try {
-		const response = await fetch(
-			`http://localhost:3000/news?id=${ctx.query.id}`
-		);
-		const news = await response.json();
+// 		const response = await fetch(
+// 			`http://localhost:3000/news?id=${ctx.query.id}`
+// 		);
+// 		const news = await response.json();
+		const news = db.news;
+		
+// 		if (news.errors) {
+// 			return { notFound: true };
+// 		}
 
-		if (news.errors) {
-			return { notFound: true };
-		}
-
-		if (!news) {
-			return { notFound: true };
-		}
+// 		if (!news) {
+// 			return { notFound: true };
+// 		}
 		return {
 			props: {
 				news,

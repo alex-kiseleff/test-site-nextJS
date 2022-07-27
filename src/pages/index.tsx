18e50e19492e4b10fb5ef8903ea839c19e221040
Alexey <64145913+alex-kiseleff@users.noreply.github.com>
2022-07-27 21:07:44 +0400

    fix: Исправил получение данных.
    Для успешного деплоя, сделал получение данных статично, без fetch-запроса, из файла DATA_BASE.json.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@
 import { GetStaticProps, NextPage } from 'next';
 import PanelServiceComponent from '../components/PanelServiceComponent/PanelServiceComponent';
 import { Container, Section } from '../styles/pages/index.styles';
+import db from '../../DATA_BASE.json';
 
 export interface IPanelService {
 	id: string;
@@ -32,16 +33,17 @@ const MainComponent: NextPage<IData> = ({ panelsServices }): JSX.Element => {
 
 export const getStaticProps: GetStaticProps = async () => {
 	try {
-		const response = await fetch('http://localhost:3000/panels-services');
-		const panelsServices = await response.json();
+// 		const response = await fetch('http://localhost:3000/panels-services');
+// 		const panelsServices = await response.json();
+		const panelsServices = db['panels-services'];
 	
-		if (panelsServices.errors) {
-			return { notFound: true };
-		}
+// 		if (panelsServices.errors) {
+// 			return { notFound: true };
+// 		}
 
-		if (!panelsServices) {
-			return { notFound: true };
-		}
+// 		if (!panelsServices) {
+// 			return { notFound: true };
+// 		}
 
 		return {
 			props: {

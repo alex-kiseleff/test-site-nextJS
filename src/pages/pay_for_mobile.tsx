64398d0b7d9c25edc3eaf00a05ae2bd64c49c38c
Alexey <64145913+alex-kiseleff@users.noreply.github.com>
2022-07-31 19:20:18 +0400

    Update pay_for_mobile.tsx

--- src/pages/pay_for_mobile.tsx
+++ src/pages/pay_for_mobile.tsx
@@ -34,7 +34,7 @@ const PayForMobileComponent: NextPage<IData> = ({ operators }): JSX.Element => {
 
 export const getStaticProps: GetStaticProps = async () => {
 	try {
-		const response = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/api/db`);
+		const response = await fetch(`${process.env.API_HOST}/api/db`);
 		const data = await response.json();
 
 		if (data.errors) {
